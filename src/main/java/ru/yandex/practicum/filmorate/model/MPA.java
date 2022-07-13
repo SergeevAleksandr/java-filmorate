47d90b6b6d3202b8de43ad4904d46482a49f3851
SergeevAleksandr <aa.sergeev.eltreit@yandex.ru>
2022-07-13 21:16:36 +0400

    добавил анатацию

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -3,11 +3,14 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class MPA {
     private String name;
     private long id;
+    public MPA(long id) {
+        this.id = id;
+    }
 }
